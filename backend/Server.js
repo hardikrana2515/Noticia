3825daecdf5e3a4fe97f6eb0ed04f22475765e69
hardikrana2515 <hardik25rana@gmail.com>
2025-06-07 15:21:01 +0530

    initial commit of backend project structure

--- backend/Server.js
+++ backend/Server.js
@@ -40,5 +40,5 @@ app.use("/iNotebook/Notes", noteRouter);
 
 //Server listening
 app.listen(PORT, () => {
-  console.log(`Server is running on  http://localhost:${PORT}`)
-});
+  // console.log(`Server is running on  http://localhost:${PORT}`)
+}); 
